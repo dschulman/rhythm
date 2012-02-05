308c067283f97419034a57bbc62d57d8ce38dfe2
Daniel Schulman <danieljschulman@gmail.com>
2012-02-05 16:22:53 -0500

    GazeGenerator has a parameterized model

--- src/rhythm/GazeGenerator.java
+++ src/rhythm/GazeGenerator.java
@@ -5,6 +5,29 @@ import static rhythm.Features.INFORMATION_STRUCTURE;
 import java.util.Iterator;
 
 public class GazeGenerator extends Processor {
+	public static enum Param {
+		ThemeStart, RhemeEnd;
+	}
+	
+	public static final Model<Param> Torres1997 = new Model<Param>() {
+		public double value(Sentence s, Param param) {
+			switch (param) {
+			case ThemeStart: return 0.7;
+			case RhemeEnd: return 0.73;
+			default: return 0;
+			}
+		}
+	}; 
+	
+	private final Model<Param> m;
+	public GazeGenerator(Model<Param> m) {
+		this.m = m;
+	}
+	
+	public GazeGenerator() {
+		this(Torres1997);
+	}
+	
 	public void process(Sentence s) {
 		boolean first = true;
 		Iterator<Interval> it = s.get(INFORMATION_STRUCTURE).iterator();
@@ -20,12 +43,12 @@ public class GazeGenerator extends Processor {
 	private void generate(Sentence s, Interval info, boolean isStart, boolean isEnd) {
 		switch (info.get(Features.INFORMATION)) {
 		case Theme:
-			if (isStart || Math.random()<0.7)
+			if (isStart || Math.random()<m.value(s, Param.ThemeStart))
 				gaze(s, info, "AWAY");
 			break;
 			
 		case Rheme:
-			if (isEnd || Math.random()<0.73)
+			if (isEnd || Math.random()<m.value(s, Param.RhemeEnd))
 				gaze(s, info, "TOWARDS");
 			break;
 		}
